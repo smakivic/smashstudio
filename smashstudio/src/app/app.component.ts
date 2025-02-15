6493ef6ebd4b0835a83f907d2978101e64215e47
stefan <stefan.makivic99@gmail.com>
2025-02-15 17:31:46 +0100

    galerija gotova

--- smashstudio/src/app/app.component.ts
+++ smashstudio/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from '@angular/common';
+import { GalleryComponent } from "./components/gallery/gallery.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TranslateModule, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, TranslateModule, CommonModule, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ smashstudio/src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,34 @@
+import { Component , AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-gallery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.scss'
+})
+export class GalleryComponent implements AfterViewInit{
+  images = Array.from({ length: 9 }, (_, i) => i + 1);
+
+  @ViewChild('track', { static: false }) track!: ElementRef;
+
+  ngAfterViewInit() {
+    const track = this.track.nativeElement;
+    let scrollAmount = 0;
+
+    function scrollGallery() {
+      scrollAmount += 0.3; // Slower scrolling speed
+      if (scrollAmount >= track.scrollWidth / 2) {
+        scrollAmount = 0;
+      }
+      track.style.transform = `translateX(-${scrollAmount}px)`;
+      requestAnimationFrame(scrollGallery);
+    }
+
+    // Duplicate images for continuous loop
+    const clone = track.cloneNode(true);
+    track.parentNode.appendChild(clone);
+
+    requestAnimationFrame(scrollGallery);
+  }
+}
